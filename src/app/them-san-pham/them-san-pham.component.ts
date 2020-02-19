52f5544016e2d2d88af55998760c031da4f5e08c
hailita1 <hailit2306@gmail.com>
2020-02-19 21:38:39 +0700

    font end

---
+++ src/app/them-san-pham/them-san-pham.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-them-san-pham',
+  templateUrl: './them-san-pham.component.html',
+  styleUrls: ['./them-san-pham.component.scss']
+})
+export class ThemSanPhamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
